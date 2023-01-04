0046fb756dd3dac7bae0395414521611c44fd3a8
anshal1 <101871035+anshal1@users.noreply.github.com>
2023-01-04 12:14:42 +0530

    Project resumed

--- src/ImageCard/Card4/Card4.jsx
+++ src/ImageCard/Card4/Card4.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
 import "./Card4.css";
-const Card4 = (IMG) => {
+const Card4 = ({ IMG }) => {
+    const previewImageLoad =(image)=>{
+        const TempArry =[];
+        const SplitText = image?.split("/");
+        console.log(SplitText);
+        let newText
+    }
+  useEffect(() => {
+    // console.log(IMG);
+    IMG.forEach(element => {
+        previewImageLoad(element.image);
+    });
+  }, [IMG]);
   return (
     <>
       <h1 className="card_info">
@@ -9,7 +22,19 @@ const Card4 = (IMG) => {
       <p className="card_info">Card 1</p>
       <p className="card_info">Hover on the image</p>
       {IMG.length !== 0 ? (
-        <div>Fourth Card</div>
+        <div id="card_holder">
+          {IMG?.map((image, index) => {
+            return (
+              <div
+                className="main-card4-container"
+                key={index}
+                style={{ "--image": `url(${image.image})` }}
+              >
+                Hello World
+              </div>
+            );
+          })}
+        </div>
       ) : (
         <h1>No Images available</h1>
       )}
